dee8c90918567ee3767cb872002c3edd383bcf3a
Harshita Saraswat <harshitasaraswat20@gmail.com>
2024-06-29 18:31:19 +0530

    added verbos, logging and history flags to invoker

--- Commandline_Preference_Management/src/invoker.py
+++ Commandline_Preference_Management/src/invoker.py
@@ -1,12 +1,24 @@
 from .commands import Command
+import FreeCAD
+from typing import List, Any
 
 class PreferenceCommandInvoker:
+    __runtime_history: List[Command] = []
 
-    def __init__(self, verbose: bool = False) -> None:
+    def __init__(self, verbose: bool = False, history: bool = False, logging: bool = False) -> None:
         self.verbose = verbose
+        self.history = history
+        self.logging = logging
 
-    def execute_command(self, cmd: Command):
+    def execute_command(self, cmd: Command) -> Any:
         result = cmd.execute()
+        if self.history:
+            self.__runtime_history.append(cmd)
         if self.verbose:
             print(f"{cmd} -> {result}")
+        if self.logging:
+            FreeCAD.Console.PrintLog(str(cmd))
         return result
+    
+    def print_history(self) -> List[str]:
+        return [str(cmd) for cmd in self.__runtime_history]
