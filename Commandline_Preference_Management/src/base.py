33a14195cc59e398f6571931f79d689dd0bb0f70
Harshita Saraswat <harshitasaraswat20@gmail.com>
2024-07-24 18:44:40 +0530

    added help strings for each argument

--- Commandline_Preference_Management/src/base.py
+++ Commandline_Preference_Management/src/base.py
@@ -6,12 +6,15 @@ from .constants import DType, Scope
 class CommandLineInterface(ArgumentParser):
     def __init__(self, args):
         super().__init__(description="Commandline Preference Management")
-        self.add_argument("mode", choices=["create", "read", "update", "delete"])
-        self.add_argument("parameter_group_path")
-        self.add_argument("-s", "--scope", choices=[s.name for s in Scope], default=Scope.User)
-        self.add_argument("-n", "--name")
-        self.add_argument("-v", "--value")
-        self.add_argument("-d", "--dtype", choices=[d.name for d in DType])
+        self.add_argument(
+            "mode", choices=["create", "read", "update", "delete"],
+            help="The mode in which the command is to be run.\n`create`: To add/create a new parameter.\n`read`: To get/read a parameter.\n`update`: To update/modify a parameter value.\n`delete`: To remove/delete exinting parameter"
+        )
+        self.add_argument("parameter_group_path", help="The path of the parameter in the config. eg: `BaseApp/Preference/OpenGL`")
+        self.add_argument("-s", "--scope", choices=[s.name for s in Scope], default=Scope.User, help="The scope of preference manipulation.")
+        self.add_argument("-n", "--name", help="The name of the Preference in the given `parameter_group_path`")
+        self.add_argument("-v", "--value", help="The value of the Preference for the given `name`")
+        self.add_argument("-d", "--dtype", choices=[d.name for d in DType], help="The datatype of the Preference you want to access for the given `parameter_group_path`")
 
         self.args = self.parse_args(args)
         if self.args.dtype == "String": return
